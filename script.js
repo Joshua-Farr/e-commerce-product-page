bc766b702567641e194282c27bc87f79ba8db834
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2023-07-06 01:37:50 -0500

    Beginning work on desktop layout

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const customerCart = document.getElementById("shopping-cart-card");
 const navigationMenu = document.getElementById("main-nav-menu");
 const shoppingCartDisplayArea = document.getElementById("cart-details");
+const numItems = document.getElementById("num-items-to-add-to-cart");
+
+let imageIndex = 0;
+let productImages = new Array(
+    "./images/image-product-1.jpg",
+    "./images/image-product-2.jpg",
+    "./images/image-product-3.jpg",
+    "./images/image-product-4.jpg")
+console.log(productImages);
+
+
+
+
 
 let shoppingCart = [];
 let numCartItems = 0;
@@ -21,27 +34,30 @@ document.addEventListener("click", function(e){
     }else if(e.target.id ==="subtract-one"){
         if(numCartItems){
             numCartItems--;
-            document.getElementById("num-items-to-add-to-cart").innerText = numCartItems;
+            numItems.innerText = numCartItems;
         }
 
     }else if(e.target.id ==="add-one"){
             numCartItems++;
-            document.getElementById("num-items-to-add-to-cart").innerText = numCartItems;
+            numItems.innerText = numCartItems;
+    }else if(e.target.id === "next-item" || e.target.id ==="previous-item"){
+        console.log("changing image!")
+        changeImage(e);
     }
 });
 
 
 
-function addToCart(name, imgUrl, price, quantity){
-    const item = {
-        itemName: name,
-        itemImg: imgUrl,
-        itemPrice: price,
-        itemQuantity:quantity
-    }
-    return item;
-}
-
+// function addToCart(name, imgUrl, price, quantity){
+//     const item = {
+//         itemName: name,
+//         itemImg: imgUrl,
+//         itemPrice: price,
+//         itemQuantity:quantity
+//     }
+//     return item;
+// }
+ 
 
 function addItemToShoppingCart(item){
     shoppingCart.push(addToCart(item)); //???? Not sure...
@@ -53,7 +69,7 @@ function calculateTotalPrice(price, quantity){
 
 function renderShoppingCart(){
 
-    console.log("rendering shopping cart")
+    // console.log("rendering shopping cart")
     shoppingCartDisplayArea.innerHTML +=
     `<div class="order-details">
         <img src="./images/image-product-1-thumbnail.jpg" class="item-thumbnail" alt="">
@@ -66,6 +82,47 @@ function renderShoppingCart(){
   <button class="checkout-button">Checkout</button>`
 
 }
+
+
+
+function changeImage(e){
+    const currentImage = document.getElementById("product-img");
+
+    if(e.target.id === "next-item"){
+        if(imageIndex === 3){
+            imageIndex = 0;
+        } else{
+            imageIndex++;  
+        }
+    }else if(e.target.id === "previous-item"){
+        if(imageIndex === 0){
+            imageIndex = 3;
+        } else {
+            imageIndex--;
+        }
+    }
+    currentImage.src = productImages[imageIndex];
+}
+
+
+
+function addToCart(){
+    if(numItems.innerText){
+        
+
+
+        numItems.innerText = 0;
+    }
+}
+
+
+
+
+
+
+
+
+
 
 
 
